df0710804726ff014deaf47e56ad896f69e50226
GubernievS <tyman@ya.ru>
2024-08-13 15:05:45 +0300

    fix

--- setup/setup.sh
+++ setup/setup.sh
@@ -16,10 +16,11 @@ export APT_LISTCHANGES_FRONTEND=none
 #
 # Установка:
 # 1. Устанавливать только на Debian 10
-# 2. Загрузить папку setup на сервер в папку root по SFTP (например через программу FileZilla)
-# 3. В консоли под root выполнить:
+# 2. Загрузить и распаковать архив https://github.com/GubernievS/AntiZapret-VPN/archive/refs/heads/main.zip
+# 3. Загрузить из архива папку setup на сервер в папку root по SFTP (например через программу FileZilla)
+# 4. В консоли под root выполнить:
 # chmod +x setup/setup.sh && setup/setup.sh
-# 4. Скопировать файл antizapret-client-udp.ovpn или antizapret-client-tcp.ovpn с сервера из папки /root/easy-rsa-ipsec/CLIENT_KEY
+# 5. Скопировать файл antizapret-client-udp.ovpn и antizapret-client-tcp.ovpn с сервера из папки /root/easy-rsa-ipsec/CLIENT_KEY
 #
 
 #
