5f306afb364970d3ce17e326485dc83fb10dcc1b
GubernievS <tyman@ya.ru>
2024-08-19 23:43:37 +0300

    Update Enable-OpenVPN-DCO.sh

--- Enable-OpenVPN-DCO.sh
+++ Enable-OpenVPN-DCO.sh
@@ -10,5 +10,9 @@ systemctl stop openvpn-server@antizapret-udp
 systemctl stop openvpn-server@antizapret-tcp
 modprobe -r ovpn_dco_v2
 modprobe ovpn_dco_v2
+sed -i '/ncp-ciphers\|data-ciphers/d' /etc/openvpn/server/antizapret-udp.conf
+sed -i '/ncp-ciphers\|data-ciphers/d' /etc/openvpn/server/antizapret-tcp.conf
+echo -e "data-ciphers \"AES-128-GCM:AES-256-GCM\"" >> /etc/openvpn/server/antizapret-udp.conf
+echo -e "data-ciphers \"AES-128-GCM:AES-256-GCM\"" >> /etc/openvpn/server/antizapret-tcp.conf
 systemctl start openvpn-server@antizapret-udp
 systemctl start openvpn-server@antizapret-tcp
