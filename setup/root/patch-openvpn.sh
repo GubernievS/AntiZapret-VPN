00e5e7b15c5bc47c21a79acc9419fe1e43f2d54d
GubernievS <tyman@ya.ru>
2024-09-12 18:37:52 +0300

    Update patch-openvpn.sh

--- setup/root/patch-openvpn.sh
+++ setup/root/patch-openvpn.sh
@@ -1,28 +1,63 @@
 #!/bin/bash
 #
-# Патч для обхода ПОЛНОЙ блокировки протокола OpenVPN на ТСПУ
+# Патч для обхода блокировки протокола OpenVPN
 # Работает только для UDP соединений
-# Если подключение блокируется с каких то определенных серверов, а с других работает - патч скорее всего не поможет
 #
 # chmod +x patch-openvpn.sh && ./patch-openvpn.sh
 #
 set -e
-apt-get update && apt-get full-upgrade -y && apt-get autoremove -y
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get full-upgrade -y -o Dpkg::Options::="--force-confdef"
+apt-get autoremove -y
 version=$(openvpn --version | head -n 1 | awk '{print $2}')
-apt-get install -y curl tar perl build-essential libssl-dev pkg-config libsystemd-dev libpam0g-dev automake libnl-genl-3-dev libcap-ng-dev
+DEBIAN_FRONTEND=noninteractive apt-get install -y curl tar perl build-essential libssl-dev pkg-config libsystemd-dev libpam0g-dev automake libnl-genl-3-dev libcap-ng-dev
 rm -rf /root/openvpn
 mkdir -p /root/openvpn
 curl -L -o openvpn.tar.gz https://build.openvpn.net/downloads/releases/openvpn-$version.tar.gz
 tar --strip-components=1 -xvzf openvpn.tar.gz -C /root/openvpn
 rm -f /root/openvpn.tar.gz
-mv /root/openvpn/src/openvpn/socket.h /root/openvpn/src/openvpn/socket
-perl -0777 -pe 's/(link_socket_write_udp\(struct link_socket \*sock,\s*struct buffer \*buf,\s*struct link_socket_actual \*to\)\s*{)/$1\n    uint8_t opcode = *BPTR(buf) >> 3;\nif (opcode == 7\n    || opcode == 8\n    || opcode == 10)\n{\n    uint8_t stuffing_data[] = {0x01, 0x00, 0x00, 0x00, 0x01};\n    size_t stuffing_len = sizeof(stuffing_data);\n    struct buffer stuffing_buf = clone_buf(buf);\n    buf_clear(&stuffing_buf);\n    buf_write(&stuffing_buf, stuffing_data, stuffing_len);\n#ifdef _WIN32\n    link_socket_write_win32(sock, &stuffing_buf, to);\n#else\n    link_socket_write_udp_posix(sock, &stuffing_buf, to);\n#endif\n    free_buf(&stuffing_buf);\n}/gs' /root/openvpn/src/openvpn/socket > /root/openvpn/src/openvpn/socket.h
+sed -i '/link_socket_write_udp(struct link_socket \*sock/,/\/\* write a TCP or UDP packet to link \*\//c\
+link_socket_write_udp(struct link_socket *sock,\
+                      struct buffer *buf,\
+                      struct link_socket_actual *to)\
+{\
+    uint16_t stuffing_sent = 0;\
+    uint8_t opcode = *BPTR(buf) >> 3;\
+if (opcode == 7 || opcode == 8 || opcode == 10)\
+{\
+    uint8_t stuffing_data[] = {0x01, 0x00, 0x00, 0x00, 0x01};\
+    size_t stuffing_len = sizeof(stuffing_data);\
+    struct buffer stuffing_buf = clone_buf(buf);\
+    buf_clear(&stuffing_buf);\
+    buf_write(&stuffing_buf, stuffing_data, stuffing_len);\
+    for (int i=0; i<100; i++)\
+    {\
+#ifdef _WIN32\
+        stuffing_sent =+ link_socket_write_win32(sock, &stuffing_buf, to);\
+#else\
+        stuffing_sent =+ link_socket_write_udp_posix(sock, &stuffing_buf, to);\
+#endif\
+    }\
+    free_buf(&stuffing_buf);\
+}\
+#ifdef _WIN32\
+    stuffing_sent =+ link_socket_write_win32(sock, buf, to);\
+#else\
+    stuffing_sent =+ link_socket_write_udp_posix(sock, buf, to);\
+#endif\
+    return stuffing_sent;\
+}\
+\
+\/\* write a TCP or UDP packet to link \*\/' "/root/openvpn/src/openvpn/socket.h"
 cd /root/openvpn
 chmod +x ./configure
 ./configure --enable-systemd=yes --disable-debug --disable-lzo --disable-lz4
 make
 make install
 rm -rf /root/openvpn
+sudo apt-get purge -y curl tar perl build-essential libssl-dev pkg-config libsystemd-dev libpam0g-dev automake libnl-genl-3-dev libcap-ng-dev
+sudo apt-get autoremove -y
+sudo apt-get clean
 systemctl daemon-reload
 systemctl restart openvpn-server@antizapret-udp
 systemctl restart openvpn-server@antizapret-tcp
