3d7500413099f3368fca11f973724c66f171f934
GubernievS <tyman@ya.ru>
2024-09-14 10:11:17 +0300

    Update patch-openvpn.sh

--- setup/root/patch-openvpn.sh
+++ setup/root/patch-openvpn.sh
@@ -6,11 +6,11 @@
 # chmod +x patch-openvpn.sh && ./patch-openvpn.sh
 #
 set -e
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get full-upgrade -y -o Dpkg::Options::="--force-confdef"
-apt-get autoremove -y
+apt update
+DEBIAN_FRONTEND=noninteractive apt full-upgrade -y -o Dpkg::Options::="--force-confdef"
+apt autoremove -y
 version=$(openvpn --version | head -n 1 | awk '{print $2}')
-DEBIAN_FRONTEND=noninteractive apt-get install -y curl tar perl build-essential libssl-dev pkg-config libsystemd-dev libpam0g-dev automake libnl-genl-3-dev libcap-ng-dev
+DEBIAN_FRONTEND=noninteractive apt install -y curl tar perl build-essential libssl-dev pkg-config libsystemd-dev libpam0g-dev automake libnl-genl-3-dev libcap-ng-dev
 rm -rf /root/openvpn
 mkdir -p /root/openvpn
 curl -L -o openvpn.tar.gz https://build.openvpn.net/downloads/releases/openvpn-$version.tar.gz
