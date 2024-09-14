cee26716f2f7e50d3850080785c69d09b9e66be2
GubernievS <tyman@ya.ru>
2024-09-14 05:23:19 +0300

    Update upgrade-openvpn.sh

--- setup/root/upgrade-openvpn.sh
+++ setup/root/upgrade-openvpn.sh
@@ -9,10 +9,16 @@ set -e
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://swupdate.openvpn.net/repos/repo-public.gpg | gpg --dearmor > /etc/apt/keyrings/openvpn-repo-public.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/openvpn-repo-public.gpg] https://build.openvpn.net/debian/openvpn/release/2.6 $(lsb_release -cs) main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
+
+if [ -d "/root/openvpn" ]; then
+	cd /root/openvpn
+	make uninstall
+fi
+
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get full-upgrade -y -o Dpkg::Options::="--force-confdef"
-DEBIAN_FRONTEND=noninteractive apt-get install -y --reinstall openvpn
 apt-get autoremove -y
+DEBIAN_FRONTEND=noninteractive apt-get install --reinstall -y openvpn
 echo ""
 echo "Successful upgrade OpenVPN! Rebooting..."
 reboot
