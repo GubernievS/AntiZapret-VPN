a63db46c12aad8282f538d0d167fbdad20748f6d
GubernievS <tyman@ya.ru>
2024-09-14 10:11:12 +0300

    Update enable-openvpn-dco.sh

--- setup/root/enable-openvpn-dco.sh
+++ setup/root/enable-openvpn-dco.sh
@@ -12,10 +12,10 @@ if [[ ! $version =~ ^2\.6 ]]; then
     exit 1
 fi
 
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get full-upgrade -y -o Dpkg::Options::="--force-confdef"
-apt-get autoremove -y
-DEBIAN_FRONTEND=noninteractive apt-get install -y linux-headers-$(uname -r) openvpn-dco-dkms
+apt update
+DEBIAN_FRONTEND=noninteractive apt full-upgrade -y -o Dpkg::Options::="--force-confdef"
+apt autoremove -y
+DEBIAN_FRONTEND=noninteractive apt install -y linux-headers-$(uname -r) openvpn-dco-dkms
 modprobe -r ovpn_dco_v2
 modprobe ovpn_dco_v2
 sed -i "/data-ciphers\|disable-dco/d" /etc/openvpn/server/antizapret-udp.conf
