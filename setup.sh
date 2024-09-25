d9f9805debc2fe0c3bf8386810a2f35b16196418
GubernievS <tyman@ya.ru>
2024-09-25 09:36:01 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -169,11 +169,7 @@ fi
 #
 # Добавляем AdGuard DNS в обычный VPN
 if [[ "$DNS_VPN" = "y" ]]; then
-	sed -i '/push "dhcp-option DNS 1\.1\.1\.1"/,+1c \
-	push "dhcp-option DNS 94.140.14.14"\
-	push "dhcp-option DNS 94.140.15.15"\
-	push "dhcp-option DNS 76.76.2.44"\
-	push "dhcp-option DNS 76.76.10.44"' /etc/openvpn/server/vpn*.conf
+	sed -i '/push "dhcp-option DNS 1\.1\.1\.1"/,+1c push "dhcp-option DNS 94.140.14.14"\npush "dhcp-option DNS 94.140.15.15"\npush "dhcp-option DNS 76.76.2.44"\npush "dhcp-option DNS 76.76.10.44"' /etc/openvpn/server/vpn*.conf
 fi
 
 #
