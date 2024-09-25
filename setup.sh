11589035ccd9f2219020cf04263451a9fdc34029
GubernievS <tyman@ya.ru>
2024-09-25 04:56:58 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -163,7 +163,7 @@ chmod +x /root/dnsmap/proxy.py
 #
 # Добавляем AdGuard DNS в AntiZapret VPN
 if [[ "$DNS_ANTIZAPRET" = "y" ]]; then
-	sed -i "/policy.FORWARD/i\\\tpolicy.FORWARD({'94.140.14.14', '94.140.15.15'})," /etc/knot-resolver/kresd.conf
+	sed -i "s/'1.1.1.1', '1.0.0.1'/'94.140.14.14', '94.140.15.15'/" /etc/knot-resolver/kresd.conf
 fi
 
 #
