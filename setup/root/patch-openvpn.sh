c8092db490f9c7ec1582a6a40e4f5b46a99c6251
GubernievS <tyman@ya.ru>
2024-09-26 23:16:33 +0300

    Update patch-openvpn.sh

--- setup/root/patch-openvpn.sh
+++ setup/root/patch-openvpn.sh
@@ -6,11 +6,22 @@
 # chmod +x patch-openvpn.sh && ./patch-openvpn.sh
 #
 set -e
-apt update
-DEBIAN_FRONTEND=noninteractive apt full-upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
-apt autoremove -y
+if [[ "$1" == "1" || "$1" == "2" ]]; then
+	ALGORITHM="$1"
+else
+	echo ""
+	echo "Choose a version of the anti-censorship patch for OpenVPN (UDP only):"
+	echo "    1) Strong     - Recommended for all"
+	echo "    2) Error-free - If the strong patch causes a connection error on your device or router"
+	until [[ $ALGORITHM =~ ^[1-2]$ ]]; do
+		read -rp "Version choice [1-2]: " -e -i 1 ALGORITHM
+	done
+fi
+#apt update
+#DEBIAN_FRONTEND=noninteractive apt full-upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+#apt autoremove -y
 version=$(openvpn --version | head -n 1 | awk '{print $2}')
-DEBIAN_FRONTEND=noninteractive apt install --reinstall -y curl tar perl build-essential libssl-dev pkg-config libsystemd-dev libpam0g-dev automake libnl-genl-3-dev libcap-ng-dev
+#DEBIAN_FRONTEND=noninteractive apt install --reinstall -y curl tar perl build-essential libssl-dev pkg-config libsystemd-dev libpam0g-dev automake libnl-genl-3-dev libcap-ng-dev
 rm -rf /root/openvpn
 mkdir -p /root/openvpn
 curl -L -o /root/openvpn.tar.gz https://build.openvpn.net/downloads/releases/openvpn-$version.tar.gz
@@ -21,35 +32,70 @@ link_socket_write_udp(struct link_socket *sock,\
 					struct buffer *buf,\
 					struct link_socket_actual *to)\
 {\
-	uint16_t stuffing_sent = 0;\
+#define ALGORITHM '"$ALGORITHM"'\
+	uint16_t buffer_sent = 0;\
 	uint8_t opcode = *BPTR(buf) >> 3;\
 if (opcode == 7 || opcode == 8 || opcode == 10)\
 {\
-	srand(time(NULL));\
-	for (int i=0; i<2; i++) {\
-		int stuffing_len = rand() % 91 + 10;\
-		uint8_t stuffing_data[100];\
-		for (int j=0; j<stuffing_len; j++) {\
-			stuffing_data[j] = rand() % 256;\
-		}\
-		struct buffer stuffing_buf = alloc_buf(100);\
-		buf_write(&stuffing_buf, stuffing_data, stuffing_len);\
-		for (int j=0; j<100; j++) {\
+	if (ALGORITHM == 2) {\
 #ifdef _WIN32\
-			stuffing_sent =+ link_socket_write_win32(sock, &stuffing_buf, to);\
+		buffer_sent =+ link_socket_write_win32(sock, buf, to);\
 #else\
-			stuffing_sent =+ link_socket_write_udp_posix(sock, &stuffing_buf, to);\
+		buffer_sent =+ link_socket_write_udp_posix(sock, buf, to);\
+#endif\
+	}\
+	uint16_t buffer_len = BLEN(buf);\
+	srand(time(NULL));\
+	for (int i = 0; i < 2; i++) {\
+		uint16_t data_len = rand() % (101 - buffer_len) + buffer_len;\
+		uint8_t data[data_len];\
+		if (ALGORITHM == 1 && i == 0) {\
+			data[0] = 1;\
+			data[1] = 0;\
+			data[2] = 0;\
+			data[3] = 0;\
+			data[4] = 1;\
+			for (int k = 5; k < data_len; k++) {\
+				data[k] = rand() % 256;\
+			}\
+		}\
+		else if (ALGORITHM == 1 && i != 0) {\
+			for (int k = 0; k < data_len; k++) {\
+				data[k] = rand() % 256;\
+			}\
+		}\
+		struct buffer data_buffer;\
+		if (ALGORITHM == 2) {\
+			data_buffer = clone_buf(buf);\
+			buf_read(&data_buffer, data, buffer_len);\
+			buf_clear(&data_buffer);\
+			data[0] = 40;\
+			for (int k = buffer_len; k < data_len; k++) {\
+				data[k] = rand() % 256;\
+			}\
+		}\
+		else {\
+			data_buffer = alloc_buf(data_len);\
+		}\
+		buf_write(&data_buffer, data, data_len);\
+		int data_repeat = rand() % 101 + 100;\
+		for (int j = 0; j < data_repeat; j++) {\
+#ifdef _WIN32\
+			buffer_sent =+ link_socket_write_win32(sock, &data_buffer, to);\
+#else\
+			buffer_sent =+ link_socket_write_udp_posix(sock, &data_buffer, to);\
 #endif\
 		}\
-		free_buf(&stuffing_buf);\
+		free_buf(&data_buffer);\
+		if (ALGORITHM == 1) { usleep(1000 * data_repeat); }\
 	}\
 }\
 #ifdef _WIN32\
-	stuffing_sent =+ link_socket_write_win32(sock, buf, to);\
+	buffer_sent =+ link_socket_write_win32(sock, buf, to);\
 #else\
-	stuffing_sent =+ link_socket_write_udp_posix(sock, buf, to);\
+	buffer_sent =+ link_socket_write_udp_posix(sock, buf, to);\
 #endif\
-	return stuffing_sent;\
+	return buffer_sent;\
 }\
 \
 \/\* write a TCP or UDP packet to link \*\/' "/root/openvpn/src/openvpn/socket.h"
@@ -60,7 +106,7 @@ make
 make install
 echo ""
 echo "Patch successful installation!"
-if [[ "$1" != "noreboot" ]]; then
+if [[ "$1" != "noreboot" && "$2" != "noreboot" ]]; then
 	echo "Rebooting..."
 	reboot
 fi
