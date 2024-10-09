52bca7a85cdd047148dfd90689f49c61500e281b
GubernievS <tyman@ya.ru>
2024-10-10 00:50:55 +0300

    Update parse.sh

--- setup/root/antizapret/parse.sh
+++ setup/root/antizapret/parse.sh
@@ -17,6 +17,7 @@ cd "$HERE"
 awk -F ';' '{
 	if ($2 ~ /\./ && $2 ~ /^[а-яА-Яa-zA-Z0-9\-_\.\*]+$/) {
 		gsub(/^\*\./, "", $2);	# Удаление *. в начале
+		gsub(/^www\./, "", $2);	# Удаление www. в начале
 		gsub(/\.$/, "", $2);	# Удаление . в конце
 		print $2				# Выводим только доменные имена
 	}
@@ -28,24 +29,11 @@ sed -E 's/^.*\.(.*\..*\..*\..*)$/\1/' temp/list.txt | sort -u > temp/blocked-hos
 # Подготавливаем исходные файлы для обработки
 ( sed -E '/^#/d; /^[[:space:]]*$/d; s/^[[:space:]]+//; s/[[:space:]]+$//' config/exclude-hosts-{dist,custom}.txt && cat temp/nxdomain.txt ) > temp/exclude-hosts.txt
 ( sed -E '/^#/d; /^[[:space:]]*$/d; s/^[[:space:]]+//; s/[[:space:]]+$//' config/include-hosts-{dist,custom}.txt && cat temp/blocked-hosts.txt) > temp/include-hosts.txt
-sed -E '/^#/d; /^[[:space:]]*$/d; s/^[[:space:]]+//; s/[[:space:]]+$//' config/include-ips-{dist,custom}.txt > temp/include-ips.txt
 
 # Очищаем список доменов
 awk -f scripts/getzones.awk temp/include-hosts.txt > temp/getzones.txt
 awk 'NR==FNR {exclude[$0]; next} !($0 in exclude)' temp/exclude-hosts.txt temp/getzones.txt > temp/blocked-hosts.txt
 grep -vFf <(grep -E '^([^.]*\.){0,1}[^.]*$' temp/blocked-hosts.txt | sed 's/^/./') temp/blocked-hosts.txt > result/blocked-hosts.txt
-
-# Заблокированные IP-адреса
-awk '/([0-9]{1,3}\.){3}[0-9]{1,3}\/[0-9]{1,2}/ {print $0}' temp/include-ips.txt > result/blocked-ranges.txt
-
-# Generate OpenVPN route file
-echo -n > result/openvpn-blocked-ranges.txt
-while read -r line
-do
-	IP="$(echo $line | awk -F '/' '{print $1}')"
-	MASK="$(sipcalc -- "$line" | awk '/Network mask/ {print $4; exit;}')"
-	echo $"push \"route ${IP} ${MASK}\"" >> result/openvpn-blocked-ranges.txt
-done < result/blocked-ranges.txt
 
 # Generate knot-resolver aliases
 echo 'blocked_hosts = {' > result/blocked-hosts.conf
