6753870aaf7ab51e42adb116a5abf8975bb8df80
GubernievS <tyman@ya.ru>
2024-10-13 22:17:17 +0300

    Update process.sh

--- setup/root/antizapret/process.sh
+++ setup/root/antizapret/process.sh
@@ -4,10 +4,4 @@ set -e
 cp result/blocked-hosts.conf /etc/knot-resolver/blocked-hosts.conf
 systemctl restart kresd@1.service
 
-#iptables-legacy -F azvpnwhitelist
-#while read -r line
-#do
-#    iptables-legacy -w -A azvpnwhitelist -d "$line" -j ACCEPT
-#done < result/blocked-ranges.txt
-
 exit 0
