b81a5e1eee6042a42aff59064a9c40f4df8939a1
GubernievS <tyman@ya.ru>
2024-10-14 22:42:02 +0300

    Update update.sh

--- setup/root/antizapret/update.sh
+++ setup/root/antizapret/update.sh
@@ -6,48 +6,30 @@ cd "$HERE"
 
 rm -f temp/*
 
-DUMP_LINK='https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv'
-DUMP_PATH='temp/dump.csv'
+DUMP_LINK="https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv"
+DUMP_PATH="temp/dump.csv"
 
-NXDOMAIN_LINK='https://raw.githubusercontent.com/zapret-info/z-i/master/nxdomain.txt'
-NXDOMAIN_PATH='temp/nxdomain.txt'
+NXDOMAIN_LINK="https://raw.githubusercontent.com/zapret-info/z-i/master/nxdomain.txt"
+NXDOMAIN_PATH="temp/nxdomain.txt"
 
-EXCLUDE_HOSTS_LINK='https://raw.githubusercontent.com/GubernievS/AntiZapret-VPN/main/setup/root/antizapret/config/exclude-hosts-dist.txt'
-EXCLUDE_HOSTS_PATH='config/exclude-hosts-dist.txt'
+EXCLUDE_HOSTS_LINK="https://raw.githubusercontent.com/GubernievS/AntiZapret-VPN/main/setup/root/antizapret/config/exclude-hosts-dist.txt"
+EXCLUDE_HOSTS_PATH="config/exclude-hosts-dist.txt"
 
-EXCLUDE_REGEXP_LINK='https://raw.githubusercontent.com/GubernievS/AntiZapret-VPN/main/setup/root/antizapret/config/exclude-regexp-dist.awk'
-EXCLUDE_REGEXP_PATH='config/exclude-regexp-dist.awk'
+EXCLUDE_REGEXP_LINK="https://raw.githubusercontent.com/GubernievS/AntiZapret-VPN/main/setup/root/antizapret/config/exclude-regexp-dist.awk"
+EXCLUDE_REGEXP_PATH="config/exclude-regexp-dist.awk"
 
-INCLUDE_HOSTS_LINK='https://raw.githubusercontent.com/GubernievS/AntiZapret-VPN/main/setup/root/antizapret/config/include-hosts-dist.txt'
-INCLUDE_HOSTS_PATH='config/include-hosts-dist.txt'
+INCLUDE_HOSTS_LINK="https://raw.githubusercontent.com/GubernievS/AntiZapret-VPN/main/setup/root/antizapret/config/include-hosts-dist.txt"
+INCLUDE_HOSTS_PATH="config/include-hosts-dist.txt"
 
-UPDATE_LINK='https://raw.githubusercontent.com/GubernievS/AntiZapret-VPN/main/setup/root/antizapret/update.sh'
-UPDATE_PATH='update.sh'
+UPDATE_LINK="https://raw.githubusercontent.com/GubernievS/AntiZapret-VPN/main/setup/root/antizapret/update.sh"
+UPDATE_PATH="update.sh"
 
-curl -f --fail-early --compressed -o $DUMP_PATH $DUMP_LINK || exit 1
-curl -f --fail-early --compressed -o $NXDOMAIN_PATH $NXDOMAIN_LINK || exit 1
-curl -f --fail-early --compressed -o $EXCLUDE_HOSTS_PATH $EXCLUDE_HOSTS_LINK || exit 1
-curl -f --fail-early --compressed -o $EXCLUDE_REGEXP_PATH $EXCLUDE_REGEXP_LINK || exit 1
-curl -f --fail-early --compressed -o $INCLUDE_HOSTS_PATH $INCLUDE_HOSTS_LINK || exit 1
-curl -f --fail-early --compressed -o $UPDATE_PATH $UPDATE_LINK || exit 1
-
-SIZE="$(curl -sI "$DUMP_LINK" | awk 'BEGIN {IGNORECASE=1;} /content-length/ {sub(/[ \t\r\n]+$/, "", $2); print $2}')"
-[[ "$SIZE" != "$(stat -c '%s' $DUMP_PATH)" ]] && echo "$DUMP_PATH size differs" && exit 2
-
-SIZE="$(curl -sI "$NXDOMAIN_LINK" | awk 'BEGIN {IGNORECASE=1;} /content-length/ {sub(/[ \t\r\n]+$/, "", $2); print $2}')"
-[[ "$SIZE" != "$(stat -c '%s' $NXDOMAIN_PATH)" ]] && echo "$NXDOMAIN_PATH size differs" && exit 2
-
-SIZE="$(curl -sI "$EXCLUDE_HOSTS_LINK" | awk 'BEGIN {IGNORECASE=1;} /content-length/ {sub(/[ \t\r\n]+$/, "", $2); print $2}')"
-[[ "$SIZE" != "$(stat -c '%s' $EXCLUDE_HOSTS_PATH)" ]] && echo "$EXCLUDE_HOSTS_PATH size differs" && exit 2
-
-SIZE="$(curl -sI "$EXCLUDE_REGEXP_LINK" | awk 'BEGIN {IGNORECASE=1;} /content-length/ {sub(/[ \t\r\n]+$/, "", $2); print $2}')"
-[[ "$SIZE" != "$(stat -c '%s' $EXCLUDE_REGEXP_PATH)" ]] && echo "$EXCLUDE_REGEXP_PATH size differs" && exit 2
-
-SIZE="$(curl -sI "$INCLUDE_HOSTS_LINK" | awk 'BEGIN {IGNORECASE=1;} /content-length/ {sub(/[ \t\r\n]+$/, "", $2); print $2}')"
-[[ "$SIZE" != "$(stat -c '%s' $INCLUDE_HOSTS_PATH)" ]] && echo "$INCLUDE_HOSTS_PATH size differs" && exit 2
-
-SIZE="$(curl -sI "$UPDATE_LINK" | awk 'BEGIN {IGNORECASE=1;} /content-length/ {sub(/[ \t\r\n]+$/, "", $2); print $2}')"
-[[ "$SIZE" != "$(stat -c '%s' $UPDATE_PATH)" ]] && echo "$UPDATE_PATH size differs" && exit 2
+curl -f --retry 3 --retry-delay 30 --retry-all-errors --compressed -o $DUMP_PATH $DUMP_LINK
+curl -f --retry 3 --retry-delay 30 --retry-all-errors --compressed -o $NXDOMAIN_PATH $NXDOMAIN_LINK
+curl -f --retry 3 --retry-delay 30 --retry-all-errors --compressed -o $EXCLUDE_HOSTS_PATH $EXCLUDE_HOSTS_LINK
+curl -f --retry 3 --retry-delay 30 --retry-all-errors --compressed -o $EXCLUDE_REGEXP_PATH $EXCLUDE_REGEXP_LINK
+curl -f --retry 3 --retry-delay 30 --retry-all-errors --compressed -o $INCLUDE_HOSTS_PATH $INCLUDE_HOSTS_LINK
+curl -f --retry 3 --retry-delay 30 --retry-all-errors --compressed -o $UPDATE_PATH $UPDATE_LINK
 
 iconv -f cp1251 -t utf8 temp/dump.csv > temp/list.csv
 
