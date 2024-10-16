112c9840ed6709c423277b605168bac1ab54f353
GubernievS <tyman@ya.ru>
2024-10-16 21:47:05 +0300

    Update update.sh

--- setup/root/antizapret/update.sh
+++ setup/root/antizapret/update.sh
@@ -32,14 +32,29 @@ DOALL_PATH="doall.sh"
 UPDATE_LINK="https://raw.githubusercontent.com/GubernievS/AntiZapret-VPN/main/setup/root/antizapret/update.sh"
 UPDATE_PATH="update.sh"
 
+echo "Downloading: $DUMP_PATH"
 curl -f --retry 3 --retry-delay 30 --retry-all-errors --compressed -o $DUMP_PATH $DUMP_LINK
 iconv -f cp1251 -t utf8 $DUMP_PATH > temp/list.csv
+
+echo "Downloading: $NXDOMAIN_PATH"
 curl -f --retry 3 --retry-delay 30 --retry-all-errors --compressed -o $NXDOMAIN_PATH $NXDOMAIN_LINK
+
+echo "Downloading: $EXCLUDE_HOSTS_PATH"
 curl -f --retry 3 --retry-delay 30 --retry-all-errors --compressed -o $EXCLUDE_HOSTS_PATH $EXCLUDE_HOSTS_LINK
+
+echo "Downloading: $EXCLUDE_REGEXP_PATH"
 curl -f --retry 3 --retry-delay 30 --retry-all-errors --compressed -o $EXCLUDE_REGEXP_PATH $EXCLUDE_REGEXP_LINK
+
+echo "Downloading: $INCLUDE_HOSTS_PATH"
 curl -f --retry 3 --retry-delay 30 --retry-all-errors --compressed -o $INCLUDE_HOSTS_PATH $INCLUDE_HOSTS_LINK
+
+echo "Downloading: $PARSE_PATH"
 curl -f --retry 3 --retry-delay 30 --retry-all-errors --compressed -o $PARSE_PATH $PARSE_LINK
+
+echo "Downloading: $DOALL_PATH"
 curl -f --retry 3 --retry-delay 30 --retry-all-errors --compressed -o $DOALL_PATH $DOALL_LINK
+
+echo "Downloading: $UPDATE_PATH"
 curl -f --retry 3 --retry-delay 30 --retry-all-errors --compressed -o $UPDATE_PATH $UPDATE_LINK
 
 exit 0
