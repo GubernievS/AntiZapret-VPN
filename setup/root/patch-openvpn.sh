f66cb1eb90aa966d5e552238ce212c10a3fcd0cb
GubernievS <tyman@ya.ru>
2024-10-22 07:05:54 +0300

    Update patch-openvpn.sh

--- setup/root/patch-openvpn.sh
+++ setup/root/patch-openvpn.sh
@@ -21,7 +21,7 @@ if [[ "$1" == "1" || "$1" == "2" ]]; then
 else
 	echo ""
 	echo "Choose a version of the anti-censorship patch for OpenVPN (UDP only):"
-	echo "    1) Strong     - Recommended for default"
+	echo "    1) Strong     - Recommended by default"
 	echo "    2) Error-free - If the strong patch causes a connection error on your device or router"
 	until [[ $ALGORITHM =~ ^[1-2]$ ]]; do
 		read -rp "Version choice [1-2]: " -e -i 1 ALGORITHM
