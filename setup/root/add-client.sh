0f63cf2820cc603e2e608012038284c9567d8db7
GubernievS <tyman@ya.ru>
2024-11-01 17:05:38 +0300

    Update add-client.sh

--- setup/root/add-client.sh
+++ setup/root/add-client.sh
@@ -31,11 +31,11 @@ if [[ "$TYPE" != "init" && "$TYPE" != "recreate" && "$TYPE" != "list" ]]; then
 	fi
 
 	CLIENT=$2
-	if [[ -z "$CLIENT" || ! "$CLIENT" =~ ^[a-zA-Z0-9_-]{1,18}$ ]]; then
+	if [[ -z "$CLIENT" || ! "$CLIENT" =~ ^[a-zA-Z0-9_-]{1,32}$ ]]; then
 		echo ""
 		echo "Tell me a name for the new client"
-		echo "The name client must consist of 1 to 18 alphanumeric characters, it may also include an underscore or a dash"
-		until [[ $CLIENT =~ ^[a-zA-Z0-9_-]{1,18}$ ]]; do
+		echo "The name client must consist of 1 to 32 alphanumeric characters, it may also include an underscore or a dash"
+		until [[ $CLIENT =~ ^[a-zA-Z0-9_-]{1,32}$ ]]; do
 			read -rp "Client name: " -e CLIENT
 		done
 	fi
@@ -268,7 +268,7 @@ elif [[ "$TYPE" == "init" || "$TYPE" == "recreate" ]]; then
 	# OpenVPN
 	if [[ -f /etc/openvpn/easyrsa3/pki/index.txt ]]; then
 		tail -n +2 /etc/openvpn/easyrsa3/pki/index.txt | grep "^V" | cut -d '=' -f 2 | while read -r line; do
-			if [[ "$line" =~ ^[a-zA-Z0-9_-]{1,18}$ ]]; then
+			if [[ "$line" =~ ^[a-zA-Z0-9_-]{1,32}$ ]]; then
 				/root/add-client.sh ov "$line" > /dev/null
 				echo "OpenVPN configuration files for the client '$line' have been recreated in '/root/vpn'"
 			else
@@ -282,7 +282,7 @@ elif [[ "$TYPE" == "init" || "$TYPE" == "recreate" ]]; then
 	# WireGuard/AmneziaWG
 	if [[ -f /etc/wireguard/antizapret.conf && -f /etc/wireguard/vpn.conf ]]; then
 		cat /etc/wireguard/antizapret.conf /etc/wireguard/vpn.conf | grep -E "^# Client" | cut -d '=' -f 2 | sed 's/ //g' | sort -u | while read -r line; do
-			if [[ "$line" =~ ^[a-zA-Z0-9_-]{1,18}$ ]]; then
+			if [[ "$line" =~ ^[a-zA-Z0-9_-]{1,32}$ ]]; then
 				/root/add-client.sh wg "$line" > /dev/null
 				echo "WireGuard/AmneziaWG configuration files for the client '$line' have been recreated in '/root/vpn'"
 			else
