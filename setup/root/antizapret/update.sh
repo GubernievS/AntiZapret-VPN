42c9d789c18e3b2a1cd192434cddebaa1fda18ea
GubernievS <tyman@ya.ru>
2024-11-23 12:37:10 +0300

    Update update.sh

--- setup/root/antizapret/update.sh
+++ setup/root/antizapret/update.sh
@@ -9,6 +9,8 @@ echo "Update antizapret files"
 #
 # update
 mkdir -p download
+find /root/antizapret/config -type f -name '*-custom*' -exec bash -c 'mv "$0" "${0//-custom/}"' {} \;
+find /root/antizapret/config -type f -name '*-dist*' -delete
 #
 #
 
