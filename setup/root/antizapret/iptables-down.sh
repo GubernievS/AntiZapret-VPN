2004884cde5e19bef28049b1367e56bbdfa48c76
GubernievS <tyman@ya.ru>
2024-12-04 19:17:07 +0300

    Исправления для совместимости с докером

--- setup/root/antizapret/iptables-down.sh
+++ setup/root/antizapret/iptables-down.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 exec 2>/dev/null
 
+INTERFACE=$(ip route | grep '^default' | awk '{print $5}')
+
 # filter
 iptables -w -D INPUT -m conntrack --ctstate INVALID -j DROP
 iptables -w -D FORWARD -m conntrack --ctstate INVALID -j DROP
@@ -16,23 +18,17 @@ iptables -w -F ANTIZAPRET-ACCEPT
 iptables -w -X ANTIZAPRET-ACCEPT
 
 # nat
-iptables -w -t nat -D PREROUTING ! -s 10.28.0.0/15 -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 50080
-iptables -w -t nat -D PREROUTING ! -s 10.28.0.0/15 -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 50443
-iptables -w -t nat -D PREROUTING ! -s 10.28.0.0/15 -p udp -m udp --dport 80 -j REDIRECT --to-ports 50080
-iptables -w -t nat -D PREROUTING ! -s 10.28.0.0/15 -p udp -m udp --dport 443 -j REDIRECT --to-ports 50443
-iptables -w -t nat -D PREROUTING ! -s 10.28.0.0/15 -p udp -m udp --dport 52080 -j REDIRECT --to-ports 51080
-iptables -w -t nat -D PREROUTING ! -s 10.28.0.0/15 -p udp -m udp --dport 52443 -j REDIRECT --to-ports 51443
-iptables -w -t nat -D PREROUTING -s 10.29.0.0/16 ! -d 10.29.0.1/32 -p udp -m udp --dport 53 -m u32 --u32 "0x1c&0xffcf=0x100&&0x1e&0xffff=0x1" -j DNAT --to-destination 10.29.0.1
+iptables -w -t nat -D PREROUTING -i "$INTERFACE" -p tcp --dport 80 -j REDIRECT --to-ports 50080
+iptables -w -t nat -D PREROUTING -i "$INTERFACE" -p tcp --dport 443 -j REDIRECT --to-ports 50443
+iptables -w -t nat -D PREROUTING -i "$INTERFACE" -p udp --dport 80 -j REDIRECT --to-ports 50080
+iptables -w -t nat -D PREROUTING -i "$INTERFACE" -p udp --dport 443 -j REDIRECT --to-ports 50443
+iptables -w -t nat -D PREROUTING -i "$INTERFACE" -p udp --dport 52080 -j REDIRECT --to-ports 51080
+iptables -w -t nat -D PREROUTING -i "$INTERFACE" -p udp --dport 52443 -j REDIRECT --to-ports 51443
+iptables -w -t nat -D PREROUTING -s 10.29.0.0/16 ! -d 10.29.0.1/32 -p udp --dport 53 -m u32 --u32 "0x1c&0xffcf=0x100&&0x1e&0xffff=0x1" -j DNAT --to-destination 10.29.0.1
 iptables -w -t nat -D PREROUTING -s 10.29.0.0/16 ! -d 10.30.0.0/15 -j CONNMARK --set-xmark 0x1/0xffffffff
 iptables -w -t nat -D PREROUTING -s 10.29.0.0/16 -d 10.30.0.0/15 -j ANTIZAPRET-MAPPING
 iptables -w -t nat -D POSTROUTING -s 10.28.0.0/15 -j MASQUERADE
-iptables -w -t nat -D PREROUTING ! -s 172.28.0.0/15 -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 50080
-iptables -w -t nat -D PREROUTING ! -s 172.28.0.0/15 -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 50443
-iptables -w -t nat -D PREROUTING ! -s 172.28.0.0/15 -p udp -m udp --dport 80 -j REDIRECT --to-ports 50080
-iptables -w -t nat -D PREROUTING ! -s 172.28.0.0/15 -p udp -m udp --dport 443 -j REDIRECT --to-ports 50443
-iptables -w -t nat -D PREROUTING ! -s 172.28.0.0/15 -p udp -m udp --dport 52080 -j REDIRECT --to-ports 51080
-iptables -w -t nat -D PREROUTING ! -s 172.28.0.0/15 -p udp -m udp --dport 52443 -j REDIRECT --to-ports 51443
-iptables -w -t nat -D PREROUTING -s 172.29.0.0/16 ! -d 172.29.0.1/32 -p udp -m udp --dport 53 -m u32 --u32 "0x1c&0xffcf=0x100&&0x1e&0xffff=0x1" -j DNAT --to-destination 172.29.0.1
+iptables -w -t nat -D PREROUTING -s 172.29.0.0/16 ! -d 172.29.0.1/32 -p udp --dport 53 -m u32 --u32 "0x1c&0xffcf=0x100&&0x1e&0xffff=0x1" -j DNAT --to-destination 172.29.0.1
 iptables -w -t nat -D PREROUTING -s 172.29.0.0/16 ! -d 172.30.0.0/15 -j CONNMARK --set-xmark 0x1/0xffffffff
 iptables -w -t nat -D PREROUTING -s 172.29.0.0/16 -d 172.30.0.0/15 -j ANTIZAPRET-MAPPING
 iptables -w -t nat -D POSTROUTING -s 172.28.0.0/15 -j MASQUERADE
--- setup/root/antizapret/iptables-up.sh
+++ setup/root/antizapret/iptables-up.sh
@@ -7,6 +7,8 @@ set -e
 HERE="$(dirname "$(readlink -f "${0}")")"
 cd "$HERE"
 
+INTERFACE=$(ip route | grep '^default' | awk '{print $5}')
+
 # filter
 # INPUT connection tracking
 iptables -w -A INPUT -m conntrack --ctstate INVALID -j DROP
@@ -28,16 +30,16 @@ iptables -w -A FORWARD -j REJECT --reject-with icmp-port-unreachable
 
 # nat
 # OpenVPN TCP port redirection for backup connections
-iptables -w -t nat -A PREROUTING ! -s 10.28.0.0/15 -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 50080
-iptables -w -t nat -A PREROUTING ! -s 10.28.0.0/15 -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 50443
+iptables -w -t nat -A PREROUTING -i "$INTERFACE" -p tcp --dport 80 -j REDIRECT --to-ports 50080
+iptables -w -t nat -A PREROUTING -i "$INTERFACE" -p tcp --dport 443 -j REDIRECT --to-ports 50443
 # OpenVPN UDP port redirection for backup connections
-iptables -w -t nat -A PREROUTING ! -s 10.28.0.0/15 -p udp -m udp --dport 80 -j REDIRECT --to-ports 50080
-iptables -w -t nat -A PREROUTING ! -s 10.28.0.0/15 -p udp -m udp --dport 443 -j REDIRECT --to-ports 50443
+iptables -w -t nat -A PREROUTING -i "$INTERFACE" -p udp --dport 80 -j REDIRECT --to-ports 50080
+iptables -w -t nat -A PREROUTING -i "$INTERFACE" -p udp --dport 443 -j REDIRECT --to-ports 50443
 # AmneziaWG redirection ports to WireGuard
-iptables -w -t nat -A PREROUTING ! -s 10.28.0.0/15 -p udp -m udp --dport 52080 -j REDIRECT --to-ports 51080
-iptables -w -t nat -A PREROUTING ! -s 10.28.0.0/15 -p udp -m udp --dport 52443 -j REDIRECT --to-ports 51443
+iptables -w -t nat -A PREROUTING -i "$INTERFACE" -p udp --dport 52080 -j REDIRECT --to-ports 51080
+iptables -w -t nat -A PREROUTING -i "$INTERFACE" -p udp --dport 52443 -j REDIRECT --to-ports 51443
 # DNS redirection to Knot Resolver
-iptables -w -t nat -A PREROUTING -s 10.29.0.0/16 ! -d 10.29.0.1/32 -p udp -m udp --dport 53 -m u32 --u32 "0x1c&0xffcf=0x100&&0x1e&0xffff=0x1" -j DNAT --to-destination 10.29.0.1
+iptables -w -t nat -A PREROUTING -s 10.29.0.0/16 ! -d 10.29.0.1/32 -p udp --dport 53 -m u32 --u32 "0x1c&0xffcf=0x100&&0x1e&0xffff=0x1" -j DNAT --to-destination 10.29.0.1
 # ANTIZAPRET-ACCEPT
 iptables -w -t nat -A PREROUTING -s 10.29.0.0/16 ! -d 10.30.0.0/15 -j CONNMARK --set-xmark 0x1/0xffffffff
 # ANTIZAPRET-MAPPING
