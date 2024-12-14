a164687bac4e9c10e96d0825acc4cbb6d1e79b81
GubernievS <tyman@ya.ru>
2024-12-14 13:14:02 +0300

    Update iptables-up.sh

--- setup/root/antizapret/iptables-up.sh
+++ setup/root/antizapret/iptables-up.sh
@@ -23,18 +23,28 @@ iptables -w -A INPUT -i "$INTERFACE" -p icmp -j DROP
 ip6tables -w -A INPUT -i "$INTERFACE" -p icmpv6 --icmpv6-type packet-too-big -j ACCEPT
 ip6tables -w -A INPUT -i "$INTERFACE" -p icmpv6 -j DROP
 # Attack and scan protection
-ipset create antizapret-blocklist hash:ip timeout 600
-ipset create antizapret-watchlist hash:ip,port timeout 20
-iptables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -m set ! --match-set antizapret-watchlist src,dst -m hashlimit --hashlimit-above 1/min --hashlimit-burst 3 --hashlimit-mode srcip --hashlimit-name antizapret-port --hashlimit-htable-expire 20000 -j SET --add-set antizapret-blocklist src --exist
-iptables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -m hashlimit --hashlimit-above 100/min --hashlimit-burst 1000 --hashlimit-mode srcip --hashlimit-name antizapret-conn --hashlimit-htable-expire 20000 -j SET --add-set antizapret-blocklist src --exist
-iptables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -m set --match-set antizapret-blocklist src -j DROP
-iptables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -j SET --add-set antizapret-watchlist src,dst
-ipset create antizapret-blocklist6 hash:ip timeout 600 family inet6
-ipset create antizapret-watchlist6 hash:ip,port timeout 20 family inet6
-ip6tables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -m set ! --match-set antizapret-watchlist6 src,dst -m hashlimit --hashlimit-above 1/min --hashlimit-burst 3 --hashlimit-mode srcip --hashlimit-name antizapret-port --hashlimit-htable-expire 20000 -j SET --add-set antizapret-blocklist6 src --exist
-ip6tables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -m hashlimit --hashlimit-above 100/min --hashlimit-burst 1000 --hashlimit-mode srcip --hashlimit-name antizapret-conn --hashlimit-htable-expire 20000 -j SET --add-set antizapret-blocklist6 src --exist
-ip6tables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -m set --match-set antizapret-blocklist6 src -j DROP
-ip6tables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -j SET --add-set antizapret-watchlist6 src,dst
+ipset create antizapret-block hash:ip timeout 600
+ipset create antizapret-watch hash:ip,port timeout 60
+iptables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -m set ! --match-set antizapret-watch src,dst -m hashlimit --hashlimit-above 10/hour --hashlimit-burst 10 --hashlimit-mode srcip --hashlimit-srcmask 24 --hashlimit-name antizapret-port --hashlimit-htable-expire 60000 -j SET --add-set antizapret-block src --exist
+iptables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -p udp -m set ! --match-set antizapret-watch src,dst -m hashlimit --hashlimit-above 5/hour --hashlimit-burst 5 --hashlimit-mode srcip --hashlimit-name antizapret-port-udp --hashlimit-htable-expire 60000 -j SET --add-set antizapret-block src --exist
+iptables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -p tcp -m set ! --match-set antizapret-watch src,dst -m hashlimit --hashlimit-above 5/hour --hashlimit-burst 5 --hashlimit-mode srcip --hashlimit-name antizapret-port-tcp --hashlimit-htable-expire 60000 -j SET --add-set antizapret-block src --exist
+iptables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -p udp -m hashlimit --hashlimit-above 1500/hour --hashlimit-burst 1500 --hashlimit-mode srcip --hashlimit-name antizapret-conn-udp --hashlimit-htable-expire 60000 -j SET --add-set antizapret-block src --exist
+iptables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -p tcp -m hashlimit --hashlimit-above 500/hour --hashlimit-burst 500 --hashlimit-mode srcip --hashlimit-name antizapret-conn-tcp --hashlimit-htable-expire 60000 -j SET --add-set antizapret-block src --exist
+iptables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -m set --match-set antizapret-block src -j DROP
+iptables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -j SET --add-set antizapret-watch src,dst
+iptables -w -A OUTPUT -o "$INTERFACE" -p icmp --icmp-type fragmentation-needed -j ACCEPT
+iptables -w -A OUTPUT -o "$INTERFACE" -p icmp -j DROP
+ipset create antizapret-block6 hash:ip timeout 600 family inet6
+ipset create antizapret-watch6 hash:ip,port timeout 60 family inet6
+ip6tables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -m set ! --match-set antizapret-watch6 src,dst -m hashlimit --hashlimit-above 10/hour --hashlimit-burst 10 --hashlimit-mode srcip --hashlimit-srcmask 24 --hashlimit-name antizapret-port6 --hashlimit-htable-expire 60000 -j SET --add-set antizapret-block6 src --exist
+ip6tables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -p udp -m set ! --match-set antizapret-watch6 src,dst -m hashlimit --hashlimit-above 5/hour --hashlimit-burst 5 --hashlimit-mode srcip --hashlimit-name antizapret-port-udp6 --hashlimit-htable-expire 60000 -j SET --add-set antizapret-block6 src --exist
+ip6tables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -p tcp -m set ! --match-set antizapret-watch6 src,dst -m hashlimit --hashlimit-above 5/hour --hashlimit-burst 5 --hashlimit-mode srcip --hashlimit-name antizapret-port-tcp6 --hashlimit-htable-expire 60000 -j SET --add-set antizapret-block6 src --exist
+ip6tables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -p udp -m hashlimit --hashlimit-above 1500/hour --hashlimit-burst 1500 --hashlimit-mode srcip --hashlimit-name antizapret-conn-udp6 --hashlimit-htable-expire 60000 -j SET --add-set antizapret-block6 src --exist
+ip6tables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -p tcp -m hashlimit --hashlimit-above 500/hour --hashlimit-burst 500 --hashlimit-mode srcip --hashlimit-name antizapret-conn-tcp6 --hashlimit-htable-expire 60000 -j SET --add-set antizapret-block6 src --exist
+ip6tables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -m set --match-set antizapret-block6 src -j DROP
+ip6tables -w -A INPUT -i "$INTERFACE" -m conntrack --ctstate NEW -j SET --add-set antizapret-watch6 src,dst
+ip6tables -w -A OUTPUT -o "$INTERFACE" -p icmpv6 --icmpv6-type packet-too-big -j ACCEPT
+ip6tables -w -A OUTPUT -o "$INTERFACE" -p icmpv6 -j DROP
 # FORWARD connection tracking
 iptables -w -A FORWARD -m conntrack --ctstate INVALID -j DROP
 iptables -w -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED,DNAT -j ACCEPT
