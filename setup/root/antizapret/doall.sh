b620ebb009525678fac4c228d02795e7584b3d0a
GubernievS <tyman@ya.ru>
2025-02-03 23:50:18 +0300

    Update doall.sh

--- setup/root/antizapret/doall.sh
+++ setup/root/antizapret/doall.sh
@@ -9,6 +9,7 @@ cd "$HERE"
 cat update.sh | bash
 ./parse.sh
 [[ -f "custom.sh" ]] && chmod +x custom.sh && ./custom.sh
-find /etc/openvpn/server/logs -type f ! -name "*status*" -delete
+find /etc/openvpn/server/logs -type f ! -name "*status*" -size +10M -delete
+
 
 echo "Execution time: $SECONDS seconds"
