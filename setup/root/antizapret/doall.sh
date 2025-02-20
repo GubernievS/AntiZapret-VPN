1f15ba771d06a8ad78b28c374205b703e0bcb801
GubernievS <tyman@ya.ru>
2025-02-20 23:37:18 +0300

    Update doall.sh

--- setup/root/antizapret/doall.sh
+++ setup/root/antizapret/doall.sh
@@ -6,7 +6,12 @@ SECONDS=0
 HERE="$(dirname "$(readlink -f "${0}")")"
 cd "$HERE"
 
+SUM1=$(sha256sum update.sh)
 cat update.sh | bash
+SUM2=$(sha256sum update.sh)
+if [[ "$SUM1" != "$SUM2" ]]; then
+    cat update.sh | bash
+fi
 ./parse.sh
 [[ -f "custom.sh" ]] && chmod +x custom.sh && ./custom.sh
 find /etc/openvpn/server/logs -type f -size +10M -delete
