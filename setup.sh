5b16845836595a7e791b77ba8f7829b928cccdf6
GubernievS <tyman@ya.ru>
2025-05-02 19:54:45 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -348,10 +348,7 @@ mv -f /root/antizapret/custom*.sh /tmp/antizapret/setup/root/antizapret &>/dev/n
 tar -xzf /root/backup*.tar.gz &>/dev/null || true
 rm -f /root/backup*.tar.gz &>/dev/null || true
 mv -f /root/easyrsa3 /tmp/antizapret/setup/etc/openvpn &>/dev/null || true
-mv -f /root/wireguard/antizapret.conf /tmp/antizapret/setup/etc/wireguard &>/dev/null || true
-mv -f /root/wireguard/vpn.conf /tmp/antizapret/setup/etc/wireguard &>/dev/null || true
-mv -f /root/wireguard/key /tmp/antizapret/setup/etc/wireguard &>/dev/null || true
-rm -rf /root/wireguard
+mv -f /root/wireguard /tmp/antizapret/setup/etc &>/dev/null || true
 
 #
 # Сохраняем настройки
