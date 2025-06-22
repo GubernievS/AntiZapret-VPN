c56d0b6133a894f8b8aa9a32510fe94f797b0fb4
GubernievS <tyman@ya.ru>
2025-06-22 18:46:08 +0300

    Update doall.sh

--- setup/root/antizapret/doall.sh
+++ setup/root/antizapret/doall.sh
@@ -6,14 +6,14 @@ SECONDS=0
 cd /root/antizapret
 
 SUM1="$(sha256sum update.sh)"
-cat update.sh | bash
+cat update.sh | bash -s "$1"
 SUM2="$(sha256sum update.sh)"
 if [[ "$SUM1" != "$SUM2" ]]; then
 	echo 'update.sh has been updated, restarting update.sh'
-	cat update.sh | bash
+	cat update.sh | bash -s "$1"
 fi
-./parse.sh
+./parse.sh "$1"
 find /etc/openvpn/server/logs -type f -size +10M -delete
-./custom-doall.sh
+./custom-doall.sh "$1"
 
 echo "Execution time: $SECONDS seconds"
