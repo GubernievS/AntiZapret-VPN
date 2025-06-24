324cee06d29548321a45ee9159b33bb2473877c9
GubernievS <tyman@ya.ru>
2025-06-24 22:44:57 +0300

    Update parse.sh

--- setup/root/antizapret/parse.sh
+++ setup/root/antizapret/parse.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+# Обработка ошибок
+handle_error() {
+	echo
+	echo "$(lsb_release -ds) $(uname -r) $(date --iso-8601=seconds)"
+	echo
+	echo -e "\e[1;31mError occurred at line $1 while executing: $2\e[0m"
+	exit 7
+}
+trap 'handle_error $LINENO "$BASH_COMMAND"' ERR
+
 echo "Parse AntiZapret VPN files:"
 
 export LC_ALL=C
