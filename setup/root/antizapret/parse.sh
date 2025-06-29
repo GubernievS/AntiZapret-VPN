ca5a4db9e6f554dab91cc9922d23bc54e265a080
GubernievS <tyman@ya.ru>
2025-06-29 13:47:21 +0300

    Update parse.sh

--- setup/root/antizapret/parse.sh
+++ setup/root/antizapret/parse.sh
@@ -3,11 +3,9 @@ set -e
 
 # Обработка ошибок
 handle_error() {
-	echo
 	echo "$(lsb_release -ds) $(uname -r) $(date --iso-8601=seconds)"
-	echo
-	echo -e "\e[1;31mError occurred at line $1 while executing: $2\e[0m"
-	exit 7
+	echo -e "\e[1;31mError at line $1: $2\e[0m"
+	exit 1
 }
 trap 'handle_error $LINENO "$BASH_COMMAND"' ERR
 
