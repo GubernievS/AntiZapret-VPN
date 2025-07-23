82bc7087726f4d5983c233cc2d5a39325af05d60
GubernievS <tyman@ya.ru>
2025-07-23 19:37:53 +0300

    Update parse.sh

--- setup/root/antizapret/parse.sh
+++ setup/root/antizapret/parse.sh
@@ -128,9 +128,9 @@ if [[ -z "$1" || "$1" == "host" || "$1" == "hosts" ]]; then
 	sed -e 's/\.$//' -e 's/"//g' download/domains.lst | \
 	CHARSET=UTF-8 idn --no-tld >> temp/include-hosts.txt
 
-	# Удаляем не существующие домены и поддомены: www. m. ru.
+	# Удаляем не существующие домены и поддомены
 	grep -vFxf download/nxdomain.txt temp/include-hosts.txt | \
-	sed -E 's/^(www[0-9]*|m|ru)\.//' | sort -u > temp/include-hosts2.txt
+	sed -E '/(\..*\.)/ s/^(www[0-9]*|[a-zA-Z][0-9]*|ru)\.//' | sort -u > temp/include-hosts2.txt
 
 	# Удаляем лишние домены
 	sed -e 's/$/$/' temp/include-hosts2.txt > temp/include-hosts3.txt
