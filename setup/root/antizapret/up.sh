8e8425ee8aca9d6b742560309cba1c3b9640db04
GubernievS <tyman@ya.ru>
2025-08-16 19:25:48 +0300

    Update up.sh

--- setup/root/antizapret/up.sh
+++ setup/root/antizapret/up.sh
@@ -6,15 +6,21 @@ if [[ -z "$INTERFACE" ]]; then
 	echo 'Default network interface not found!'
 	exit 1
 fi
+EXTERNAL_IP="$(ip -4 addr show dev "$INTERFACE" | grep -oP '(?<=inet\s)\d+(\.\d+){3}')"
+if [[ -z "$EXTERNAL_IP" ]]; then
+	echo 'External IPv4 address not found on default network interface!'
+	exit 1
+fi
 
-/root/antizapret/down.sh "$INTERFACE"
+/root/antizapret/down.sh "$INTERFACE" "$EXTERNAL_IP"
 
 source /root/antizapret/setup
 
 [[ "$ALTERNATIVE_IP" == "y" ]] && IP="172" || IP="10"
 
 # Clear knot-resolver cache
-echo 'cache.clear()' | socat - /run/knot-resolver/control/1
+count=$(echo 'cache.clear()' | socat - /run/knot-resolver/control/1 | grep -oE '[0-9]+' || echo 0)
+echo "DNS cache cleared: $count entries"
 
 # Network parameters modification
 sysctl -w net.ipv4.ip_forward=1
@@ -23,6 +29,13 @@ sysctl -w net.core.default_qdisc=fq || true
 sysctl -w net.ipv4.tcp_congestion_control=bbr || true
 
 # filter
+# Default policy
+iptables -w -P INPUT ACCEPT
+iptables -w -P FORWARD ACCEPT
+iptables -w -P OUTPUT ACCEPT
+ip6tables -w -P INPUT ACCEPT
+ip6tables -w -P FORWARD ACCEPT
+ip6tables -w -P OUTPUT ACCEPT
 # INPUT connection tracking
 iptables -w -I INPUT 1 -m conntrack --ctstate INVALID -j DROP
 ip6tables -w -I INPUT 1 -m conntrack --ctstate INVALID -j DROP
@@ -32,14 +45,28 @@ ip6tables -w -I FORWARD 1 -m conntrack --ctstate INVALID -j DROP
 # OUTPUT connection tracking
 iptables -w -I OUTPUT 1 -m conntrack --ctstate INVALID -j DROP
 ip6tables -w -I OUTPUT 1 -m conntrack --ctstate INVALID -j DROP
-# FORWARD VPN traffic
-iptables -w -I FORWARD 2 -d ${IP}.28.0.0/15 -j ACCEPT
-iptables -w -I FORWARD 3 -s ${IP}.28.0.0/15 -j ACCEPT
+# Restrict forwarding
+if [[ "$RESTRICT_FORWARD" == "y" ]]; then
+	{
+		echo "create antizapret-forward hash:net -exist"
+		echo "flush antizapret-forward"
+		while read -r line; do
+			echo "add antizapret-forward $line"
+		done < /root/antizapret/result/forward-ips.txt
+	} | ipset restore
+	iptables -w -I FORWARD 2 -s ${IP}.29.0.0/16 -o "$INTERFACE" -m connmark --mark 0x1 -m set ! --match-set antizapret-forward dst -j REJECT --reject-with icmp-host-prohibited
+fi
 # Attack and scan protection
 if [[ "$ATTACK_PROTECTION" == "y" ]]; then
-	ipset list antizapret-block &>/dev/null || ipset create antizapret-block hash:ip timeout 600
-	ipset list antizapret-watch &>/dev/null || ipset create antizapret-watch hash:ip,port timeout 60
-	ipset list antizapret-allow &>/dev/null || ipset create antizapret-allow hash:net
+	{
+		echo "create antizapret-allow hash:net -exist"
+		echo "flush antizapret-allow"
+		while read -r line; do
+			echo "add antizapret-allow $line"
+		done < /root/antizapret/result/allow-ips.txt
+	} | ipset restore
+	ipset create antizapret-block hash:ip timeout 600 -exist
+	ipset create antizapret-watch hash:ip,port timeout 60 -exist
 	iptables -w -I INPUT 2 -i "$INTERFACE" -p icmp --icmp-type echo-request -j DROP
 	iptables -w -I INPUT 3 -i "$INTERFACE" -m set --match-set antizapret-allow src -j ACCEPT
 	iptables -w -I INPUT 4 -i "$INTERFACE" -m conntrack --ctstate NEW -m set ! --match-set antizapret-watch src,dst -m hashlimit --hashlimit-above 10/hour --hashlimit-burst 10 --hashlimit-mode srcip --hashlimit-srcmask 24 --hashlimit-name antizapret-scan --hashlimit-htable-expire 60000 -j SET --add-set antizapret-block src --exist
@@ -48,9 +75,9 @@ if [[ "$ATTACK_PROTECTION" == "y" ]]; then
 	iptables -w -I INPUT 7 -i "$INTERFACE" -m conntrack --ctstate NEW -j SET --add-set antizapret-watch src,dst
 	iptables -w -I OUTPUT 2 -o "$INTERFACE" -p tcp --tcp-flags RST RST -j DROP
 	iptables -w -I OUTPUT 3 -o "$INTERFACE" -p icmp --icmp-type destination-unreachable -j DROP
-	ipset list antizapret-block6 &>/dev/null || ipset create antizapret-block6 hash:ip timeout 600 family inet6
-	ipset list antizapret-watch6 &>/dev/null || ipset create antizapret-watch6 hash:ip,port timeout 60 family inet6
-	ipset list antizapret-allow6 &>/dev/null || ipset create antizapret-allow6 hash:net family inet6
+	ipset create antizapret-allow6 hash:net family inet6 -exist
+	ipset create antizapret-block6 hash:ip timeout 600 family inet6 -exist
+	ipset create antizapret-watch6 hash:ip,port timeout 60 family inet6 -exist
 	ip6tables -w -I INPUT 2 -i "$INTERFACE" -p icmpv6 --icmpv6-type echo-request -j DROP
 	ip6tables -w -I INPUT 3 -i "$INTERFACE" -m set --match-set antizapret-allow6 src -j ACCEPT
 	ip6tables -w -I INPUT 4 -i "$INTERFACE" -m conntrack --ctstate NEW -m set ! --match-set antizapret-watch6 src,dst -m hashlimit --hashlimit-above 10/hour --hashlimit-burst 10 --hashlimit-mode srcip --hashlimit-srcmask 64 --hashlimit-name antizapret-scan6 --hashlimit-htable-expire 60000 -j SET --add-set antizapret-block6 src --exist
@@ -91,10 +118,14 @@ iptables -w -t nat -A PREROUTING -s ${IP}.29.8.0/24 ! -d ${IP}.29.8.1/32 -p udp 
 iptables -w -t nat -A PREROUTING -s ${IP}.29.0.0/22 ! -d ${IP}.29.0.1/32 -p tcp --dport 53 -j DNAT --to-destination ${IP}.29.0.1
 iptables -w -t nat -A PREROUTING -s ${IP}.29.4.0/22 ! -d ${IP}.29.4.1/32 -p tcp --dport 53 -j DNAT --to-destination ${IP}.29.4.1
 iptables -w -t nat -A PREROUTING -s ${IP}.29.8.0/24 ! -d ${IP}.29.8.1/32 -p tcp --dport 53 -j DNAT --to-destination ${IP}.29.8.1
-# ANTIZAPRET-MAPPING
-iptables -w -t nat -nL ANTIZAPRET-MAPPING &>/dev/null || iptables -w -t nat -N ANTIZAPRET-MAPPING
+# Restrict forwarding
+if [[ "$RESTRICT_FORWARD" == "y" ]]; then
+	iptables -w -t nat -A PREROUTING -s ${IP}.29.0.0/16 ! -d ${IP}.30.0.0/15 -j CONNMARK --set-mark 0x1
+fi
+# Mapping fake IP to real IP
+iptables -w -t nat -S ANTIZAPRET-MAPPING &>/dev/null || iptables -w -t nat -N ANTIZAPRET-MAPPING
 iptables -w -t nat -A PREROUTING -s ${IP}.29.0.0/16 -d ${IP}.30.0.0/15 -j ANTIZAPRET-MAPPING
-# MASQUERADE VPN traffic
-iptables -w -t nat -A POSTROUTING -s ${IP}.28.0.0/15 -j MASQUERADE
+# SNAT VPN
+iptables -w -t nat -A POSTROUTING -s ${IP}.28.0.0/15 -o "$INTERFACE" -j SNAT --to-source "$EXTERNAL_IP"
 
 /root/antizapret/custom-up.sh
