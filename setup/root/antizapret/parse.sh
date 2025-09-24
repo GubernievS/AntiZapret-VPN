2e017112e97cbfdd0c9d7496f6f562e5dd62b5db
GubernievS <tyman@ya.ru>
2025-09-24 07:46:35 +0300

    Update parse.sh

--- setup/root/antizapret/parse.sh
+++ setup/root/antizapret/parse.sh
@@ -170,7 +170,7 @@ if [[ -z "$1" || "$1" == "host" || "$1" == "hosts" ]]; then
 	| CHARSET=UTF-8 idn --no-tld >> temp/include-hosts.txt
 
 	# Удаляем домены казино и букмекеров
-	grep -Evi '[ck]a+[szc3]+[iley1]+n+[o0]|[vw][uy]+[l1]+[kc]a+n|[vw]a+[vw]+a+d+a|x-*bet|most-*bet|leon-*bet|rio-*bet|mel-*bet|ramen-*bet|marathon-*bet|max-*bet|bet-*win|gg-*bet|spin-*bet|banzai-*bet|1iks-*bet|x-*slot|sloto-*zal|max-*slot|bk-*leon|gold-*fishka|play-*fortuna|dragon-*money|poker-*dom|1-*win|crypto-*bos|free-*spin|fair-*spin|no-*deposit|igrovye|avtomaty|bookmaker|zerkalo|official|slottica|sykaaa|admiral-*x|x-*admiral' temp/include-hosts.txt > temp/include-hosts2.txt \
+	grep -Evi '[ck]a+[szc3]+[iley1]+n+[o0]|[vw][uy]+[l1]+[kc]a+n|[vw]a+[vw]+a+d+a|x-*bet|most-*bet|leon-*bet|rio-*bet|mel-*bet|ramen-*bet|marathon-*bet|max-*bet|bet-*win|gg-*bet|spin-*bet|banzai-*bet|1iks-*bet|x-*slot|sloto-*zal|max-*slot|bk-*leon|gold-*fishka|play-*fortuna|dragon-*money|poker-*dom|1-*win|crypto-*bos|free-*spin|fair-*spin|no-*deposit|igrovye|avtomaty|bookmaker|zerkalo|official|slottica|sykaaa|admiral-*x|x-*admiral|pinup-*bet' temp/include-hosts.txt > temp/include-hosts2.txt \
 	|| cp temp/include-hosts.txt temp/include-hosts2.txt
 
 	# Удаляем не существующие домены
