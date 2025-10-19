3c8fd7c50721e753ae466acc49887c011827d71b
GubernievS <tyman@ya.ru>
2025-10-19 22:05:31 +0300

    Update update.sh

--- setup/root/antizapret/update.sh
+++ setup/root/antizapret/update.sh
@@ -26,16 +26,16 @@ PARSE_PATH="parse.sh"
 DOALL_LINK="https://raw.githubusercontent.com/GubernievS/AntiZapret-VPN/main/setup/root/antizapret/doall.sh"
 DOALL_PATH="doall.sh"
 
-HOSTS_LINK_1="https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv.gz"
-HOSTS_PATH_1="download/dump.csv.gz"
-#HOSTS_LINK_1="https://svn.code.sf.net/p/zapret-info/code/dump.csv"
-#HOSTS_PATH_1="download/dump.csv"
+#HOSTS_LINK_1="https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv.gz"
+#HOSTS_PATH_1="download/dump.csv.gz"
+HOSTS_LINK_1="https://svn.code.sf.net/p/zapret-info/code/dump.csv"
+HOSTS_PATH_1="download/dump.csv"
 
 HOSTS_LINK_2="https://antifilter.download/list/domains.lst"
 HOSTS_PATH_2="download/domains.lst"
 
-NXDOMAIN_LINK="https://raw.githubusercontent.com/zapret-info/z-i/master/nxdomain.txt"
-#NXDOMAIN_LINK="https://svn.code.sf.net/p/zapret-info/code/nxdomain.txt"
+#NXDOMAIN_LINK="https://raw.githubusercontent.com/zapret-info/z-i/master/nxdomain.txt"
+NXDOMAIN_LINK="https://svn.code.sf.net/p/zapret-info/code/nxdomain.txt"
 NXDOMAIN_PATH="download/nxdomain.txt"
 
 RPZ_LINK="https://raw.githubusercontent.com/GubernievS/AntiZapret-VPN/main/setup/root/antizapret/download/rpz.txt"
