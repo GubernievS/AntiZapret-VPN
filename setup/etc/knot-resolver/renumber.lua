02c69b80a1b955d4f4435d20e96d51cc53814cb0
GubernievS <tyman@ya.ru>
2025-11-03 22:31:58 +0300

    Create renumber.lua

---
+++ setup/etc/knot-resolver/renumber.lua
@@ -0,0 +1,6 @@
+-- IP address renumbering
+return nil
+--return {
+--	{'A.B.C.D/M', 'X.Y.Z.W'},	-- Source subnet -> destination subnet
+--	{'A.B.C.D/M', 'X.Y.Z.W!'}	-- Source subnet -> single address
+--}
