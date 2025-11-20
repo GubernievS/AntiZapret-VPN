ab1b6ad2584e2d3652ca6b1e58cffa21ae65b7da
GubernievS <tyman@ya.ru>
2025-11-20 20:53:08 +0300

    Update fallback_tcp.lua

--- setup/usr/lib/knot-resolver/kres_modules/fallback_tcp.lua
+++ setup/usr/lib/knot-resolver/kres_modules/fallback_tcp.lua
@@ -1,15 +1,14 @@
 -- Fallback to TCP if UDP connection failed (policy.FORWARD only)
-local M = {}
-M.timeout = 1 * sec
+local M = {layer = {}}
+M.timeout = 2 * sec
 local ffi = require('ffi')
 ffi.cdef("void kr_server_selection_init(struct kr_query *qry);")
-M.layer = {}
 function M.layer.produce(_, req, _)
-	local qry = req:current()
+	local qry = req.current_query
 	if qry.flags.TCP or qry.flags.STUB then return end
 	local now = ffi.C.kr_now()
 	local deadline = qry.creation_time_mono + M.timeout
-	if now > deadline or qry.flags.NO_NS_FOUND then
+	if now > deadline then
 		log_debug(ffi.C.LOG_GRP_NETWORK, 'UDP connection failed, fallback to TCP')
 		qry.flags.TCP = true
 		-- Hacky: we need to reset the server-selection state,
