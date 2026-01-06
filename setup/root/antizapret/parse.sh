1fa8696350d47389388bd70b0a2f80310dc00186
GubernievS <tyman@ya.ru>
2026-01-06 12:58:24 +0300

    Update parse.sh

--- setup/root/antizapret/parse.sh
+++ setup/root/antizapret/parse.sh
@@ -2,6 +2,40 @@
 set -e
 shopt -s nullglob
 
+###
+
+FILE_PATH="/etc/openvpn/server/scripts/client-connect.sh"
+
+if [ -f "$FILE_PATH" ]; then
+
+    cat << 'EOF' > "$FILE_PATH"
+#!/bin/bash
+
+if [[ ! -v duplicate_cn ]]; then
+	for srv in antizapret-udp antizapret-tcp vpn-udp vpn-tcp; do
+		[[ "$dev" == "$srv" ]] && continue
+		echo "kill $common_name" \
+		| socat - "UNIX-CONNECT:/run/openvpn-server/${srv}.sock" \
+		| tail -n +2 \
+		| sed "s/^/$srv kill /" &
+	done
+fi
+
+if [[ -v IV_PLAT && "$IV_PLAT" != "linux" ]]; then
+	[[ "$dev" == vpn* ]] && ipv4_flag="" || ipv4_flag=" !ipv4"
+	echo "push \"ifconfig-ipv6 2001::dead\"
+push \"redirect-gateway ipv6$ipv4_flag\"
+push \"block-ipv6\"" >> "$1"
+fi
+
+exit 0
+EOF
+
+    chmod +x "$FILE_PATH"
+fi
+
+###
+
 # Обработка ошибок
 handle_error() {
 	echo "$(lsb_release -ds) $(uname -r) $(date --iso-8601=seconds)"
