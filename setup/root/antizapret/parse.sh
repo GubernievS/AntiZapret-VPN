56316ba0eeba2d5a65a59aa79d5a8beb69252b3c
GubernievS <tyman@ya.ru>
2026-01-11 19:45:55 +0300

    Update parse.sh

--- setup/root/antizapret/parse.sh
+++ setup/root/antizapret/parse.sh
@@ -2,44 +2,6 @@
 set -e
 shopt -s nullglob
 
-###
-
-FILE_PATH="/etc/openvpn/server/scripts/client-connect.sh"
-
-if [ -f "$FILE_PATH" ]; then
-
-    cat << 'EOF' > "$FILE_PATH"
-#!/bin/bash
-
-if [[ ! -v duplicate_cn ]]; then
-	for srv in antizapret-udp antizapret-tcp vpn-udp vpn-tcp; do
-		[[ "$dev" == "$srv" ]] && continue
-		echo "kill $common_name" \
-		| socat - "UNIX-CONNECT:/run/openvpn-server/${srv}.sock" \
-		| tail -n +2 \
-		| sed "s/^/$srv kill /" &
-	done
-fi
-
-if [[ "$IV_PLAT" == "android" || "$IV_PLAT" == "ios" ]]; then
-	echo "push \"ifconfig-ipv6 fd00::dead/64 fd00::1\"
-push \"route-ipv6 ::/1\"
-push \"route-ipv6 8000::/1\"
-push \"block-ipv6\"" >> "$1"
-elif [[ "$IV_PLAT" == "win" || "$IV_PLAT" == "mac" ]]; then
-	echo "push \"ifconfig-ipv6 fd00::dead/64 fd00::1\"
-push \"route-ipv6 ::/1\"
-push \"route-ipv6 8000::/1\"" >> "$1"
-fi
-
-exit 0
-EOF
-
-    chmod +x "$FILE_PATH"
-fi
-
-###
-
 # Обработка ошибок
 handle_error() {
 	echo "$(lsb_release -ds) $(uname -r) $(date --iso-8601=seconds)"
