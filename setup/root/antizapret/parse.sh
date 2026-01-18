dde61746e645ce39df40ef73f746b92d8a3b9e7c
GubernievS <tyman@ya.ru>
2026-01-18 13:32:28 +0300

    Update parse.sh

--- setup/root/antizapret/parse.sh
+++ setup/root/antizapret/parse.sh
@@ -2,9 +2,47 @@
 
 ###
 
-sed -i "s/, '195.208.4.1', '195.208.5.1'//g" /etc/knot-resolver/kresd.conf
-sed -i "s/{'62.76.76.62', '62.76.62.76'}/{'62.76.76.62', '62.76.62.76', '193.58.251.251'}/" /etc/knot-resolver/kresd.conf
-systemctl restart kresd@*
+if [[ -f /etc/sysctl.d/99-antizapret.conf ]]; then
+
+echo "# AntiZapret parameters modification
+kernel.printk=3 4 1 3
+kernel.panic=1
+kernel.panic_on_oops=1
+kernel.softlockup_panic=1
+kernel.hardlockup_panic=1
+net.ipv4.ip_forward=1
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+net.ipv4.tcp_mtu_probing=1
+net.core.rmem_max=4194304
+net.core.wmem_max=4194304
+net.ipv4.tcp_rmem=4096 16384 4194304
+net.ipv4.tcp_wmem=4096 16384 4194304
+net.ipv4.tcp_no_metrics_save=1
+net.core.netdev_budget=300
+net.ipv4.tcp_fastopen=3
+net.ipv4.ip_local_port_range=10000 50000
+net.netfilter.nf_conntrack_max=65536
+net.core.netdev_budget_usecs=2000
+net.core.dev_weight=64
+net.ipv4.tcp_max_syn_backlog=1024
+net.netfilter.nf_conntrack_buckets=16384
+net.ipv4.conf.all.rp_filter=2
+net.ipv4.conf.default.rp_filter=2
+net.core.netdev_max_backlog=1000
+net.core.somaxconn=1024
+net.ipv4.tcp_syncookies=1
+net.ipv4.udp_rmem_min=16384
+net.ipv4.udp_wmem_min=16384
+net.core.optmem_max=524288
+net.ipv4.tcp_timestamps=1
+net.ipv4.tcp_tw_reuse=1
+net.ipv4.tcp_slow_start_after_idle=0
+net.netfilter.nf_conntrack_tcp_timeout_established=86400" > /etc/sysctl.d/99-antizapret.conf
+
+sysctl --system
+
+fi
 
 ###
 
