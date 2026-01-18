200ae266612dc8aadc4b5f9aa28a7730506df97b
GubernievS <tyman@ya.ru>
2026-01-18 13:08:36 +0300

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -72,32 +72,40 @@ apt-get clean
 
 # Proxy parameters modification
 echo "# Proxy parameters modification
-net.ipv4.ip_forward=1
 kernel.printk=3 4 1 3
+kernel.panic=10
+kernel.panic_on_oops=1
+kernel.softlockup_panic=1
+kernel.hardlockup_panic=1
+net.ipv4.ip_forward=1
 net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr
 net.ipv4.tcp_mtu_probing=1
-net.core.rmem_max=16777216
-net.core.wmem_max=16777216
-net.ipv4.tcp_rmem=4096 131072 16777216
-net.ipv4.tcp_wmem=4096 16384 16777216
+net.core.rmem_max=4194304
+net.core.wmem_max=4194304
+net.ipv4.tcp_rmem=4096 16384 4194304
+net.ipv4.tcp_wmem=4096 16384 4194304
 net.ipv4.tcp_no_metrics_save=1
-net.core.netdev_budget=600
+net.core.netdev_budget=300
 net.ipv4.tcp_fastopen=3
-net.ipv4.ip_local_port_range=1024 65535
-net.netfilter.nf_conntrack_max=262144
-net.core.netdev_budget_usecs=4000
+net.ipv4.ip_local_port_range=10000 50000
+net.netfilter.nf_conntrack_max=65536
+net.core.netdev_budget_usecs=2000
 net.core.dev_weight=64
 net.ipv4.tcp_max_syn_backlog=1024
-net.netfilter.nf_conntrack_buckets=65536
+net.netfilter.nf_conntrack_buckets=16384
 net.ipv4.conf.all.rp_filter=0
 net.ipv4.conf.default.rp_filter=0
-net.core.netdev_max_backlog=2000
-net.core.somaxconn=4096
+net.core.netdev_max_backlog=1000
+net.core.somaxconn=1024
 net.ipv4.tcp_syncookies=1
 net.ipv4.udp_rmem_min=16384
 net.ipv4.udp_wmem_min=16384
-net.core.optmem_max=524288" > /etc/sysctl.d/99-proxy.conf
+net.core.optmem_max=524288
+net.ipv4.tcp_timestamps=1
+net.ipv4.tcp_tw_reuse=1
+net.ipv4.tcp_slow_start_after_idle=0
+net.netfilter.nf_conntrack_tcp_timeout_established=86400" > /etc/sysctl.d/99-proxy.conf
 
 # Disable IPv6
 echo "# Disable IPv6
