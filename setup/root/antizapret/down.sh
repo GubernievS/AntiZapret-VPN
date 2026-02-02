f42930680f4dd70de609da9adde60cba4ba68346
GubernievS <tyman@ya.ru>
2026-02-02 21:26:43 +0300

    Revert "Update down.sh"
    This reverts commit 1a87c32f4c143ab67a3659a182a445fcbeae0f49.

--- setup/root/antizapret/down.sh
+++ setup/root/antizapret/down.sh
@@ -46,7 +46,7 @@ iptables -w -D FORWARD -s $IP.28.0.0/16 -m set --match-set antizapret-torrent sr
 # Restrict forwarding
 iptables -w -D FORWARD -s $IP.29.0.0/16 -m connmark --mark 0x1 -m set ! --match-set antizapret-forward dst -j DROP
 # Client isolation
-iptables -w -D FORWARD ! -i "$DEFAULT_INTERFACE" ! -i "$OUT_INTERFACE" -d $IP.28.0.0/15 -j DROP
+iptables -w -D FORWARD ! -i "$DEFAULT_INTERFACE" -d $IP.28.0.0/15 -j DROP
 iptables -w -D FORWARD -d $IP.28.0.0/15 -j ACCEPT
 # Attack and scan protection
 iptables -w -D INPUT -i "$DEFAULT_INTERFACE" -p icmp --icmp-type echo-request -j DROP
