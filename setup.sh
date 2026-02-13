6c03cda7ecff413ce84f8a49d6a0bfad2f61e258
GubernievS <tyman@ya.ru>
2026-02-13 06:43:21 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -330,11 +330,11 @@ apt-get install --reinstall -y curl gpg
 mkdir -p /etc/apt/keyrings
 
 # Добавим репозиторий Knot Resolver
-curl -fsSL https://pkg.labs.nic.cz/gpg -o /etc/apt/keyrings/cznic-labs-pkg.gpg
+curl -fsSL --connect-timeout 30 https://pkg.labs.nic.cz/gpg -o /etc/apt/keyrings/cznic-labs-pkg.gpg
 echo "deb [signed-by=/etc/apt/keyrings/cznic-labs-pkg.gpg] https://pkg.labs.nic.cz/knot-resolver $(lsb_release -cs) main" > /etc/apt/sources.list.d/cznic-labs-knot-resolver.list
 
 # Добавим репозиторий OpenVPN
-curl -fsSL https://swupdate.openvpn.net/repos/repo-public.gpg | gpg --yes --dearmor -o /etc/apt/keyrings/openvpn-repo-public.gpg
+curl -fsSL --connect-timeout 30 https://swupdate.openvpn.net/repos/repo-public.gpg | gpg --yes --dearmor -o /etc/apt/keyrings/openvpn-repo-public.gpg
 echo "deb [signed-by=/etc/apt/keyrings/openvpn-repo-public.gpg] https://build.openvpn.net/debian/openvpn/release/2.6 $(lsb_release -cs) main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
 
 # Добавим репозиторий Debian Backports
