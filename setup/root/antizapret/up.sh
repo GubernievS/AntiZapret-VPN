493ac5f02378522688e0778aa460b910d702a78d
GubernievS <tyman@ya.ru>
2026-02-17 09:43:56 +0300

    Update up.sh

--- setup/root/antizapret/up.sh
+++ setup/root/antizapret/up.sh
@@ -106,16 +106,16 @@ ip6tables -w -I OUTPUT 1 -m conntrack --ctstate INVALID -j DROP
 # Torrent guard
 if [[ "$TORRENT_GUARD" == "y" ]]; then
 	ipset create antizapret-torrent hash:ip timeout 60 -exist
-	iptables -w -I FORWARD 2 -s $IP.28.0.0/16 -p tcp -m string --string "GET " --algo kmp --to 100 -m string --string "info_hash=" --algo bm -m string --string "peer_id=" --algo bm -m string --string "port=" --algo bm -j SET --add-set antizapret-torrent src --exist
-	iptables -w -I FORWARD 3 -s $IP.28.0.0/16 -p udp -m string --string "BitTorrent protocol" --algo kmp --to 100 -j SET --add-set antizapret-torrent src --exist
-	iptables -w -I FORWARD 4 -s $IP.28.0.0/16 -p udp -m string --string "d1:ad2:id20:" --algo kmp --to 100 -j SET --add-set antizapret-torrent src --exist
+	iptables -w -I FORWARD 2 -s $IP.28.0.0/16 -p tcp -m string --string 'GET ' --algo kmp --to 100 -m string --string 'info_hash=' --algo bm -m string --string 'peer_id=' --algo bm -m string --string 'port=' --algo bm -j SET --add-set antizapret-torrent src --exist
+	iptables -w -I FORWARD 3 -s $IP.28.0.0/16 -p udp -m string --string 'BitTorrent protocol' --algo kmp --to 100 -j SET --add-set antizapret-torrent src --exist
+	iptables -w -I FORWARD 4 -s $IP.28.0.0/16 -p udp -m string --string 'd1:ad2:id20:' --algo kmp --to 100 -j SET --add-set antizapret-torrent src --exist
 	iptables -w -I FORWARD 5 -s $IP.28.0.0/16 -m set --match-set antizapret-torrent src -j DROP
 fi
 # Restrict forwarding
 if [[ "$RESTRICT_FORWARD" == "y" ]]; then
 	{
-		echo "create antizapret-forward hash:net -exist"
-		echo "flush antizapret-forward"
+		echo 'create antizapret-forward hash:net -exist'
+		echo 'flush antizapret-forward'
 		while read -r line; do
 			echo "add antizapret-forward $line"
 		done < result/forward-ips.txt
@@ -131,8 +131,8 @@ fi
 # Attack and scan protection
 if [[ "$ATTACK_PROTECTION" == "y" ]]; then
 	{
-		echo "create antizapret-allow hash:net -exist"
-		echo "flush antizapret-allow"
+		echo 'create antizapret-allow hash:net -exist'
+		echo 'flush antizapret-allow'
 		while read -r line; do
 			echo "add antizapret-allow $line"
 		done < result/allow-ips.txt
