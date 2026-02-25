2d6a60614d639b79bf82970e5001c9cf8813695e
GubernievS <tyman@ya.ru>
2026-02-25 22:36:07 +0300

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -97,6 +97,7 @@ apt-get purge -y tuned
 apt-get purge -y sysstat
 apt-get purge -y acpid
 apt-get purge -y fwupd
+apt-get purge -y watchdog
 
 # SSH protection включён
 if [[ "$SSH_PROTECTION" == 'y' ]]; then
