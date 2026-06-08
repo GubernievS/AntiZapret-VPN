7be7434bc3ce7a497c5b570ddb6e180ac02b6025
GubernievS <26377864+GubernievS@users.noreply.github.com>
2026-06-08 11:56:01 +0300

    Update parse.sh

--- setup/root/antizapret/parse.sh
+++ setup/root/antizapret/parse.sh
@@ -257,11 +257,14 @@ if [[ -z "$1" || "$1" == 'host' || "$1" == 'hosts' || "$1" == 'noclear' || "$1" 
 		cp -f result/proxy.rpz /etc/knot-resolver/proxy.rpz.tmp
 		mv -f /etc/knot-resolver/proxy.rpz.tmp /etc/knot-resolver/proxy.rpz
 		sleep 5
-		if [[ "$1" != 'noclear' && "$1" != 'noclean' ]]; then
-			# Очищаем кэш Knot Resolver
-			count="$(echo 'cache.clear()' | socat - /run/knot-resolver/control/1 | grep -oE '[0-9]+' || echo 0)"
-			echo "DNS cache cleared: $count entries"
-		fi
+	fi
+
+	# Очищаем кэш Knot Resolver
+	if [[ "$1" != 'noclear' && "$1" != 'noclean' ]]; then
+		count="$(echo 'cache.clear()' | socat - /run/knot-resolver/control/1 | grep -oE '[0-9]+' || echo 0)"
+		echo "AntiZapret DNS cache cleared: $count entries"
+		count="$(echo 'cache.clear()' | socat - /run/knot-resolver/control/2 | grep -oE '[0-9]+' || echo 0)"
+		echo "VPN DNS cache cleared: $count entries"
 	fi
 fi
 
