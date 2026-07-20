9f9fec31678958b339c0b180f568ef41231eaf19
GubernievS <26377864+GubernievS@users.noreply.github.com>
2026-07-20 15:49:21 +0300

    Update doall.sh

--- setup/root/antizapret/doall.sh
+++ setup/root/antizapret/doall.sh
@@ -3,7 +3,7 @@ set -e
 
 SECONDS=0
 
-find /etc/openvpn/server/logs -type f -size +100M -exec truncate -s 0 {} + || true
+[[ -d /etc/openvpn/server/logs ]] && find /etc/openvpn/server/logs -type f -size +100M -exec truncate -s 0 {} +
 cd /root/antizapret
 SUM1="$(sha256sum update.sh)"
 cat update.sh | bash -s "$1"
