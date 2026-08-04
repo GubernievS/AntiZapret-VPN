0c2e1a9bdb37ae41a44691bf2631fc4a40fe1c0a
GubernievS <26377864+GubernievS@users.noreply.github.com>
2026-08-05 02:46:03 +0300

    Update up.sh

--- setup/root/antizapret/up.sh
+++ setup/root/antizapret/up.sh
@@ -321,16 +321,16 @@ else
 fi
 
 # Network tuning
-TXQUEUELEN="${TXQUEUELEN:-1000}"
 SEGMENTATION_OFFLOAD="${SEGMENTATION_OFFLOAD:-off}"
+TXQUEUELEN="${TXQUEUELEN:-10000}"
 CPU_MASK=$(printf '%x' $(( (1 << $(nproc)) - 1 )))
 for dev in $(ls /sys/class/net); do
 	[[ "$dev" == "lo" || "$dev" == *docker* ]] && continue
-	# Set TX queue length
-	ip link set "$dev" txqueuelen "$TXQUEUELEN"
 	# Packet segmentation offload
-	ethtool -K "$dev" tso "$SEGMENTATION_OFFLOAD" gso "$SEGMENTATION_OFFLOAD" gro "$SEGMENTATION_OFFLOAD" rx-udp-gro-forwarding "$SEGMENTATION_OFFLOAD"
+	ethtool -K "$dev" tso "$SEGMENTATION_OFFLOAD" gso "$SEGMENTATION_OFFLOAD" gro "$SEGMENTATION_OFFLOAD"
 	if [[ -e "/sys/class/net/$dev/device" ]]; then
+		# Set TX queue length
+		ip link set "$dev" txqueuelen "$TXQUEUELEN"
 		# Enable SoftIRQ CPU balance
 		echo "$CPU_MASK" | tee /sys/class/net/$dev/queues/rx-*/rps_cpus >/dev/null
 	else
