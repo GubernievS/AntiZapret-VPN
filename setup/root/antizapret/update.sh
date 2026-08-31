b91b34981112212bbd105a3b5ca3691fe9623ffd
GubernievS <26377864+GubernievS@users.noreply.github.com>
2026-08-31 11:57:21 +0300

    Update update.sh

--- setup/root/antizapret/update.sh
+++ setup/root/antizapret/update.sh
@@ -30,7 +30,7 @@ PARSE_PATH=parse.sh
 DOALL_LINK=https://raw.githubusercontent.com/GubernievS/AntiZapret-VPN/main/setup/root/antizapret/doall.sh
 DOALL_PATH=doall.sh
 
-DOMAIN_LINK=https://raw.githubusercontent.com/bol-van/rulist/refs/heads/main/reestr_hostname.txt
+DOMAIN_LINK=https://raw.githubusercontent.com/bol-van/rulist/heads/main/reestr_hostname.txt
 DOMAIN_PATH=download/bol-van-domain.txt
 
 DOMAIN2_LINK=https://antifilter.download/list/domains.lst
